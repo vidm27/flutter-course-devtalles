da321a06d2b4647804665f864a744b75dd19ffa2
vidm27 <davidmoran464@gmail.com>
2023-06-14 14:01:20 -0500

    Home view que renderiza los menu item mediante demanda

---
+++ widgets_app/lib/presentation/screens/home/home_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_item.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter + Material'),
+      ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ListView.builder(
+        itemCount: appMenuItems.length,
+        prototypeItem: ListTile(title: Text(appMenuItems.first.title)),
+        itemBuilder: (context, index) {
+          final menuItem = appMenuItems[index];
+
+          return ListTile(
+            title: Text(menuItem.title),
+            subtitle: Text(menuItem.subTitle),
+            leading: Icon(menuItem.icon),
+            trailing: Icon(
+              Icons.arrow_forward_ios_rounded,
+              color: colors.primary,
+            ),
+            onTap: () {},
+          );
+        });
+  }
+}
